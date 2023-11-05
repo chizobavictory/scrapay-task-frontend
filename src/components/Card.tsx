1fbc2d06ed9978a59b5946b213ada98c48e02abf
Chizoba Victory <chizoba.victory1@gmail.com>
2023-11-05 13:08:20 +0100

    feat(core): add edit modal in Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,15 @@
-import React from "react";
-import { Box, Text, Button, Flex, IconButton, Grid } from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  Box,
+  Text,
+  Button,
+  Flex,
+  IconButton,
+  Grid,
+} from "@chakra-ui/react";
 import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import DeleteBookModal from "./DeleteBookModal";
+import EditBookModal from "./EditBookModal";
 
 interface CardProps {
   book: {
@@ -13,6 +22,25 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ book, onEdit, onDelete }) => {
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
+  const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
+
+  const handleEditClick = () => {
+    setEditModalOpen(true);
+  };
+
+  const handleDeleteClick = () => {
+    setDeleteModalOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setEditModalOpen(false);
+  };
+
+  const closeDeleteModal = () => {
+    setDeleteModalOpen(false);
+  };
+
   return (
     <Grid templateColumns="1fr" gap="2" borderWidth="1px" borderRadius="lg" overflow="hidden" p="2">
       <Text fontSize="xl" fontWeight="bold" mb="2">
@@ -20,12 +48,30 @@ const Card: React.FC<CardProps> = ({ book, onEdit, onDelete }) => {
       </Text>
       <Text>{book.description}</Text>
       <Flex mt="2">
-      <Button onClick={onEdit} marginRight="2">          <EditIcon />
+        <Button onClick={handleEditClick} marginRight="2">
+          <EditIcon />
         </Button>
-        <Button onClick={onDelete}>
+        <Button onClick={handleDeleteClick}>
           <DeleteIcon />
         </Button>
       </Flex>
+
+      {isEditModalOpen && (
+        <EditBookModal
+          isOpen={isEditModalOpen}
+          onClose={closeEditModal}
+          book={book}
+          onEdit={onEdit}
+        />
+      )}
+
+      {isDeleteModalOpen && (
+        <DeleteBookModal
+          isOpen={isDeleteModalOpen}
+          onClose={closeDeleteModal}
+          onDelete={onDelete}
+        />
+      )}
     </Grid>
   );
 };
---
+++ src/components/EditBookModal.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+
+interface EditBookModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  book: {
+    id: number;
+    name: string;
+    description: string;
+  };
+  onEdit: () => void;
+}
+
+const EditBookModal: React.FC<EditBookModalProps> = ({ isOpen, onClose, book, onEdit }) => {
+  const [name, setName] = useState(book.name);
+  const [description, setDescription] = useState(book.description);
+
+  const handleEditBook = () => {
+    // You can validate the input here if needed
+    // Then, call the onEdit function to edit the book
+    onEdit();
+    onClose();
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Edit Book</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <FormControl>
+            <FormLabel>Book Name</FormLabel>
+            <Input value={name} onChange={(e) => setName(e.target.value)} />
+          </FormControl>
+          <FormControl mt={4}>
+            <FormLabel>Description</FormLabel>
+            <Input value={description} onChange={(e) => setDescription(e.target.value)} />
+          </FormControl>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={handleEditBook}>
+            Save
+          </Button>
+          <Button onClick={onClose}>Cancel</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default EditBookModal;
