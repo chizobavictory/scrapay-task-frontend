d614b506d01d929f063a1d0df09a3801c80e45e7
Chizoba Victory <chizoba.victory1@gmail.com>
2023-11-05 13:07:40 +0100

    feat(core): add delete modal in Card component

---
+++ src/components/DeleteBookModal.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Box, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, Text } from "@chakra-ui/react";
+
+interface DeleteBookModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onDelete: () => void;
+}
+
+const DeleteBookModal: React.FC<DeleteBookModalProps> = ({ isOpen, onClose, onDelete }) => {
+  const confirmDelete = () => {
+    onDelete();
+    onClose();
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Delete Book</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text>Are you sure you want to delete this book?</Text>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="red" mr={3} onClick={confirmDelete}>
+            Delete
+          </Button>
+          <Button onClick={onClose}>Cancel</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default DeleteBookModal;
