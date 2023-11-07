b0d6340d6a0fd939247dbea93512ae8a6932b68d
Chizoba Victory <chizoba.victory1@gmail.com>
2023-11-07 09:50:44 +0100

    fix(env): loaded env to config

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,7 @@ import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 import settings from "./config";
-import dotenv from "dotenv";
-dotenv.config();
+
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
