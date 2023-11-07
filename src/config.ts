20489d97fae40bf84f2d30edda57a18cd794fcfb
Chizoba Victory <chizoba.victory1@gmail.com>
2023-11-07 08:43:38 +0100

    fix(env): loaded env to config

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+const settings = {
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN || "",
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || "",
+};
+
+export default settings;
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
+import settings from "./config";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <Auth0Provider
-    domain='dev-xnhb5uromijr0654.us.auth0.com'
-    clientId="nmeS8Hj0eKXIsvShbhgWZvNdCyULVwHg"
+    domain={settings.AUTH0_DOMAIN}
+    clientId={settings.AUTH0_CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
