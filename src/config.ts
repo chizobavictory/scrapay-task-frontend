c50db264cf8625e39bd90e8db157bb5aaf35a2c7
Chizoba Victory <chizoba.victory1@gmail.com>
2023-11-07 09:44:35 +0100

    fix(env): loaded env to config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const settings = {
-  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN || "",
-  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || "",
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN || "dev-xnhb5uromijr0654.us.auth0.com",
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || "nmeS8Hj0eKXIsvShbhgWZvNdCyULVwHg",
 };
 
 export default settings;
